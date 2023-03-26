32336be7f11bb203019ce6c8f49cd5e87b14c29e
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-03-25 23:35:40 -0400

    passes core

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 console.log('Write your code here');
+let url = ('http://localhost:3000/current-exhibits')
+let commentSection = document.getElementById("comments-section")
+let newComment = document.getElementById("comment-form")
+let buyTicket = document.getElementById("buy-tickets-button")
+let ticketsBought = document.getElementById("tickets-bought")
+
+let counter = 0
+
+
+
+fetch(url)
+.then(response => response.json())
+.then(data => renderArtExhibit(data))
+
+function renderArtExhibit(artArray) {
+    artArray.forEach(art => {
+        let artTitle = document.getElementById("exhibit-title")
+        artTitle.innerText = art.title
+
+        let artDesc = document.getElementById("exhibit-description")
+        artDesc.innerText = art.description
+
+        let artImage = document.getElementById("exhibit-image")
+        artImage.src = art.image
+
+        art.comments.forEach(comment => {
+            let p = document.createElement('p')
+            p.innerText = comment
+            commentSection.append(p)
+        })
+    })
+
+}
+
+newComment.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let p = document.createElement('p')
+    p.innerText = e.target['comment-input'].value
+    commentSection.append(p)
+})
+
+buyTicket.addEventListener('click', () => {
+    counter++
+    ticketsBought.innerText = `${counter} Tickets Bought`
+})
